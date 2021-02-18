a645c691b57e689bc4b118a442d65521a2e6317d
Deanna C. Hooper <deanna.hooper@rwth-aachen.de>
2021-02-18 09:21:39 +0100

    Added changes to WiggleZ proposed by J. Renk in pull request 152 (with minor edits).

--- montepython/likelihood_class.py
+++ montepython/likelihood_class.py
@@ -2049,7 +2049,7 @@ class Likelihood_mpk(Likelihood):
                 P_lin = np.interp(self.kh, self.k_fid, P)
 
         elif self.use_sdssDR7:
-            kh = np.logspace(math.log(1e-3),math.log(1.0),num=(math.log(1.0)-math.log(1e-3))/0.01+1,base=math.exp(1.0)) # k in h/Mpc
+            kh = np.geomspace(1e-3,1,num=int((math.log(1.0)-math.log(1e-3))/0.01)+1) # k in h/Mpc
             # Rescale the scaling factor by the fiducial value for h divided by the sampled value
             # h=0.701 was used for the N-body calibration simulations
             scaling = scaling * (0.701/h)
